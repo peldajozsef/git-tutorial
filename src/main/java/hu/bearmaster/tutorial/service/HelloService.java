bfe2f2d5020a10a895266b24fe3d763c87458415
Példa József <pelda.jozsef@mail.com>
2021-10-14 19:35:17 +0200

    String formatter összefűzés helyett

--- src/main/java/hu/bearmaster/tutorial/service/HelloService.java
+++ src/main/java/hu/bearmaster/tutorial/service/HelloService.java
@@ -3,7 +3,7 @@ package hu.bearmaster.tutorial.service;
 public class HelloService {
 
     public static String sayHello(String name) {
-        return "Hello " + name + "!";
+        return String.format("Hello %s!", name);
     }
 
 }
